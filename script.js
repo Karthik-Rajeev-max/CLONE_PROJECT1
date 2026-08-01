94b3cb03e02684d115df9bc3ef346d70852c76f9
karthik rajeev <karthikrajeev17@gmail.com>
2026-08-02 01:05:47 +0530

    added logo

--- script.js
+++
@@ -1,9 +0,0 @@
-function searchItem() {
-    const search = document.querySelector(".srch").value;
-
-    if (search === "") {
-        alert("Please enter something to search.");
-    } else {
-        alert("You searched for: " + search);
-    }
-}
